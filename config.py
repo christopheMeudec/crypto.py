b2a9afb645f0eafb1c23284a268230827b0a8c05
Christophe Meudec <christophemeudec@msn.com>
2026-04-19 16:03:17 +0200

    Update config to be more aggresive

--- config.py
+++ config.py
@@ -15,26 +15,26 @@ TELEGRAM_SEND_LOOP_SUMMARY = True
 
 # --- Pairs & timeframe ---
 SYMBOLS = ["BTC/USDT", "ETH/USDT"]
-TIMEFRAME = "30m"          # '1m', '5m', '15m', '1h', '4h', '1d'
+TIMEFRAME = "15m"          # '1m', '5m', '15m', '1h', '4h', '1d'
 OHLCV_LIMIT = 200         # nombre de bougies à récupérer
 
 # --- Paper trading ---
 INITIAL_CAPITAL_USDT = 100.0   # capital fictif de départ
-TRADE_ALLOCATION = 0.10           # fraction du capital par trade (10%)
+TRADE_ALLOCATION = 0.25           # fraction du capital par trade (10%)
 
 # --- Stratégie RSI ---
 RSI_PERIOD = 14
-RSI_OVERSOLD = 35         # signal BUY en dessous de ce seuil
-RSI_OVERBOUGHT = 65       # signal SELL au-dessus de ce seuil
+RSI_OVERSOLD = 40         # signal BUY en dessous de ce seuil
+RSI_OVERBOUGHT = 60       # signal SELL au-dessus de ce seuil
 
 # --- Stratégie MACD ---
-MACD_FAST = 12
-MACD_SLOW = 26
-MACD_SIGNAL = 9
+MACD_FAST = 8
+MACD_SLOW = 21
+MACD_SIGNAL = 5
 
 # --- Boucle principale ---
-LOOP_INTERVAL_SECONDS = 1800  # une itération toutes les 30 minutes (aligné sur timeframe 30m)
+LOOP_INTERVAL_SECONDS = 900  # une itération toutes les 15 minutes (aligné sur timeframe 15m)
 
 # --- Optimisation des timeframes ---
-OPTIMIZATION_TIMEFRAMES = ["30m", "1h", "4h"]
+OPTIMIZATION_TIMEFRAMES = ["15m","30m", "1h", "4h"]
 OPTIMIZATION_OHLCV_LIMIT = 1000
